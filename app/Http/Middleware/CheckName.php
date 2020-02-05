46e45a44b7dfd6f77e3c8b8b81f1f3d252428d9a
Jakub Paluszkiewicz <jakub.k.paluszkiewicz@gmail.com>
2020-02-05 20:31:49 +0100

    changes in check name middleware

--- app/Http/Middleware/CheckName.php
+++ app/Http/Middleware/CheckName.php
@@ -15,13 +15,7 @@ class CheckName
      */
     public function handle($request, Closure $next)
     {
-        $name = $request->name;
-
-        if (is_null($name)) {
-            return response(__('swapi.no_name'), 404);
-        }
-
-        if (!preg_match('/[A-Za-z0-9-_]+/', $name)) {
+        if (!preg_match('/[A-Za-z0-9-_]+/', $request->name)) {
             return response(__('swapi.wrong_name'), 404);
         }
 
--- resources/lang/en/swapi.php
+++ resources/lang/en/swapi.php
@@ -3,7 +3,6 @@
 return [
     'people_saved' => 'People have been saved in the database.',
     'not_found' => 'Person doesn\'t exists in database.',
-    'no_name' => 'Person\'s name is required.',
     'wrong_name' => 'Person\'s name should contains letters, digits and "-" or "_". If you want to provide few words split them with "_".',
     'wrong_token' => 'Wrong token',
     'empty_table' => 'Person table is empty. Run ' . route('fetch_people') . ' to insert people to database.'
